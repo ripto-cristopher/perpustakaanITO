6273c654b531e1965e15368652b5a23fb9a8e1dd
jonathanGea <jonathangea09@gmail.com>
2023-01-12 14:15:31 +0700

    revisi database create

--- settings/database.sql
+++ settings/database.sql
@@ -1,16 +1,17 @@
--- id diambil dari nisn jika siswa, NUPTK (Nomor Unik Pendidik dan Tenaga Kependidikan) jika staf atau tenaga pengajar, idanggotaperpustakaan berguna juga untuk login
+-- id diambil dari nisn/nis jika siswa, NUPTK (Nomor Unik Pendidik dan Tenaga Kependidikan) jika staf atau tenaga pengajar, idanggotaperpustakaan berguna juga untuk login
 -- email untuk pengiriman pemberintahuan (dibuat jika memungkinkan)
 -- status = { 'aktif', 'tidak aktif'}, aktif jika anggota perpustakaan masih dalam organisasi, tidak aktif jika telah keluar dari organisasi (lulus, pindah, atau dikeluarkan)
 -- kategori = { 'guru', 'siswa'}
--- password dalam bentuk hash 256 
+-- password dalam bentuk hash 256 , default tanggal lahir format (yyyymmdd)
 
 create table anggotaperpustakaan (
 id int primary key not NULL,
 nama VARCHAR ( 50 ) NOT NULL,
 email VARCHAR ( 50 ) UNIQUE NOT NULL,
-status Varchar (20) not null,
+status Varchar (20)  not null DEFAULT 'aktif',
+tanggallahir date not null,
 kategori varchar (20) not null,
-password varchar (100) NOT NULL 
+password varchar (100) NOT null 
 )
 
 create table penerbit (
@@ -37,36 +38,42 @@ nama varchar (100) not null,
 tahunTerbit int not null
 )
 
--- status = {'hilang', 'rusak', 'perbaikan','tersedia', 'dipinjma'}
+-- status = {'hilang', 'rusak', 'perbaikan','tersedia'}
 
 create table subBuku (
 id BIGSERIAL  primary key not NULL,
 idbuku BIGINT REFERENCES buku (id),
-status Varchar (20) not null,
+status Varchar (20) not null DEFAULT 'tersedia' ,
 tanggalMasuk date not null
 )
 
+-- admin login dengan id dan password
+-- id diambil dari no induk staf (asumsi staf semua ada no id)
+-- status = { 'aktif', 'tidak aktif'}, aktif jika admin perpustakaan masih dalam organisasi, tidak aktif jika telah keluar dari organisasi
+
+
 create table admin (
-id BIGSERIAL  primary key not NULL,
-username Varchar (50) unique not null,
+id int primary key not NULL,
+status Varchar (20)  not null DEFAULT 'aktif',
 nama Varchar (50) not null,
 password varchar (100) NOT NULL 
 
 )
-
 
 create table peminjaman (
 id BIGSERIAL  primary key not NULL,
 idsubbuku BIGINT REFERENCES subbuku (id),
 idadmin BIGINT REFERENCES admin (id),
 idanggotaperpustakaan bigint REFERENCES anggotaperpustakaan (id),
-tanggalpeminjaman date not null,
-bataspengembalian date not null
+tanggalpeminjaman date not null default current_date ,
+bataspengembalian date not null default current_date + interval '1 day' * 7,
+flag int not null default 1
 )
 
 create table pengembalian (
 id BIGSERIAL primary key not NULL,
-idsubbuku BIGINT REFERENCES peminjaman (id),
-tanggalpengembalian date not null,
+idsubbuku BIGINT REFERENCES subBuku (id),
+idadmin BIGINT REFERENCES admin (id),
+tanggalpengembalian date not null default current_date,
 denda int
 )
