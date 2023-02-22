8ea867a56d78c88791a2662cf262518b783c3868
jonathanGea <jonathangea09@gmail.com>
2023-02-22 08:02:09 +0700

    update

--- settings/credentials.py
+++ settings/credentials.py
@@ -1,11 +1,11 @@
-# HOST = "ec2-3-229-161-70.compute-1.amazonaws.com"
-# DATABASE = "dbl6n849eca1f6"
-# USER_POSTGRES_DB = "pnkbxhcvnxlrbo"
-# PASSWORD_POSTGRES_DB = "000ced06ebcbc3bd342f4464814a1d5686e2f2f30f5292a7d5e1c1bbb02128b7"
-# PORT = 5432
- 
-HOST = "localhost"
-DATABASE = "postgres"
-USER_POSTGRES_DB = "postgres"
-PASSWORD_POSTGRES_DB = "p"
+HOST = "ec2-3-229-161-70.compute-1.amazonaws.com"
+DATABASE = "dbl6n849eca1f6"
+USER_POSTGRES_DB = "pnkbxhcvnxlrbo"
+PASSWORD_POSTGRES_DB = "000ced06ebcbc3bd342f4464814a1d5686e2f2f30f5292a7d5e1c1bbb02128b7"
 PORT = 5432
+ 
+# HOST = "localhost"
+# DATABASE = "postgres"
+# USER_POSTGRES_DB = "postgres"
+# PASSWORD_POSTGRES_DB = "p"
+# PORT = 5432
